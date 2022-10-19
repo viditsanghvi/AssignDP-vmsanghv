7ac01dd12079fda337121b2a563410d8fa7145fd
Vidit Sanghvi <vsanghv@asu.edu>
2022-10-18 17:51:51 -0700

    Exported Visitor Diagram to java file

---
+++ NodeVisitor.java
@@ -0,0 +1,13 @@
+
+public abstract class NodeVisitor {
+
+
+	public abstract void visitProduct(Product product;
+
+
+	public abstract void visitTrading(Trading trading) ;
+
+
+	public abstract void visitFacade(Facade facade);}
+
+}
