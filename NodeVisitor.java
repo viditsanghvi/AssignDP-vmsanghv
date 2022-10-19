28e3cf1aa1b3a95eb5505111fbd35b70ed95774d
Vidit Sanghvi <vsanghv@asu.edu>
2022-10-19 14:26:09 -0700

    Main.java created

--- NodeVisitor.java
+++ NodeVisitor.java
@@ -2,12 +2,13 @@
 public abstract class NodeVisitor {
 
 
-	public abstract void visitProduct(Product product;
+	public abstract void visitProduct(Product product);
 
 
 	public abstract void visitTrading(Trading trading) ;
 
 
-	public abstract void visitFacade(Facade facade);}
-
+	public abstract void visitFacade(Facade facade);
 }
+
+
